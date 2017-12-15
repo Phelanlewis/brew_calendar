9c9cf76003eb6f494f3c4aa90de7ad8d984e1155
Phelan Lewis <phelanlewis@gmail.com>
2017-12-14 22:56:54 -0800

    added recipe and calendar

---
+++ src/components/Calendar.vue
@@ -0,0 +1,42 @@
+/* eslint-disable */
+<template>
+    <div id='app'>
+        <h1>My Calendar</h1>
+            <calendar-month 
+			:show-date='showDate'
+			@setShowDate='setShowDate'
+			class="holiday-us-traditional holiday-us-official"
+		/>
+	</div>
+</template>
+
+<script>
+  import CalendarMonth from 'vue-simple-calendar/src/components/calendar-month'
+  require('vue-simple-calendar/static/css/default.css')
+  require('vue-simple-calendar/static/css/holidays-us.css')
+
+  export default {
+    name: 'app',
+    data: function () {
+      return { showDate: new Date() }
+    },
+    components: {
+      CalendarMonth
+    },
+    methods: {
+      setShowDate (d) {
+        this.showDate = d
+      }
+    }
+}
+</script>
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		color: #2c3e50;
+		height: 67vh;
+		width: 90vw;
+		margin-left: auto;
+		margin-right: auto;
+	}
+</style>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app"> 
+    <h1>Hello I am a Beer Recipe</h1>
+    <p>Malt : I am a number</p>
+    <p>Hops : I am also a number</p>
+  </div>
+
+</template>
+<<script>
+export default {
+}
+</script>
+<<style>
+  #app{
+    padding: 2em
+  }
+</style>
+
