581d9050cf1ee0e7ebfdcec4892f3819a5c1e994
Phelan Lewis <phelanlewis@gmail.com>
2017-12-15 15:18:12 -0800

    routing between home and calendar/recipe page works

--- src/router/index.js
+++ src/router/index.js
@@ -2,26 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Calendar from '@/components/Calendar'
-import Recipe from '../components/Recipe'
+import Recipe from '@/components/Recipe'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/calendar',
-      name: 'Calendar',
-      component: Calendar
-    },
-    {
-      path: '/recipe',
-      name: 'Recipe',
-      component: Recipe
-    }
+    { path: '/', name: 'HelloWorld', component: HelloWorld },
+    {path: '/calendar', name: 'Calendar', component: Calendar},
+    { path: '/recipe', name: 'Recipe', component: Recipe }
   ]
 })
