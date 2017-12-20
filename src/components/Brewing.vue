9284dadee5d2ab298d89daaa73bdf3758097d467
Phelan Lewis <phelanlewis@gmail.com>
2017-12-19 20:53:39 -0800

    added brew.vue and server

---
+++ src/components/Brewing.vue
@@ -0,0 +1,32 @@
+<<template>
+  <div class="hello">
+    <h1>{{ msg }} </h1>
+  </div>
+</template>
+
+<<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Brew Day!'
+    }
+  }
+}
+</script>
+<<style>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+    padding: 1em;
+  }
+</style>
